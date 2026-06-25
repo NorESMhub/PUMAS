3dcbc24ad89884ee9e79d6be8c96df4bb5245df3
Ove Haugvaldstad <ovehaugv@runbox.no>
2026-06-25 15:19:55 +0200

    fix bug of NCAL unit conversion such that i correspond to the diagnostic.

--- micro_pumas_v1.F90
+++ micro_pumas_v1.F90
@@ -1872,7 +1872,7 @@ subroutine micro_pumas_tend (                                            &
      do i=1,mgncol
         if (qc(i,k) >= qsmall) then
            nc(i,k) = max(nc(i,k) + npccn(i,k)*deltat, 0._r8)
-           ncal(i,k) = npccn(i,k)
+           ncal(i,k) = npccn(i,k)*deltat*rho(i,k)
         else
            ncal(i,k) = 0._r8
         end if
